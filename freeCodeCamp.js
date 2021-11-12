526046a9e846096e5588464ab8407222d4053758
Vasilievkosta <vasilievkosta@gmail.com>
2021-11-12 19:54:23 +0200

    start incubator repository

---
+++ freeCodeCamp.js
@@ -0,0 +1,15 @@
+// iterate-odd-numbers-with-a-for-loop
+const myArray = [];
+
+for (let i = 1; i < 10; i += 2) {
+  myArray.push(i);
+}
+
+// count-backwards-with-a-for-loop
+const myArray = [];
+
+for (let i = 1; i < 6; i++) {
+  myArray.push(i);
+}
+
+//
