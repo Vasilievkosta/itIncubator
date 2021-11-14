1dcf6d6f4e7639c01a3493541e31b3389b897359
Vasilievkosta <vasilievkosta@gmail.com>
2021-11-14 18:19:40 +0200

    add task profile-lookup

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -58,7 +58,7 @@ if (n <= 0) return 0;
 	} 
 }
 
-// умножьте первые nэлементы массива, чтобы создать произведение этих элементов.
+// умножьте первые n элементы массива, чтобы создать произведение этих элементов.
 
 function multiply(arr, n) {
     if (n <= 0) {
@@ -67,6 +67,58 @@ function multiply(arr, n) {
       return multiply(arr, n - 1) * arr[n - 1];
     }
 }
+
+// profile-lookup
+
+// У нас есть массив объектов, представляющих разных людей в наших списках контактов.
+// Для вас заранее написана lookUpProfileфункция, которая принимает nameи свойство ( prop) в качестве аргументов.
+// Функция должна проверять, nameявляется ли это фактическим контактом, firstNameи данное свойство ( prop) является свойством этого контакта.
+// Если оба верны, то верните «значение» этого свойства.
+// Если nameне соответствует ни одному контакту, вернуть строку No such contact.
+// Если propне соответствует каким-либо действительным свойствам найденного контакта, nameверните строку No such property.
+
+const contacts = [
+  {
+    firstName: "Akira",
+    lastName: "Laine",
+    number: "0543236543",
+    likes: ["Pizza", "Coding", "Brownie Points"],
+  },
+  {
+    firstName: "Harry",
+    lastName: "Potter",
+    number: "0994372684",
+    likes: ["Hogwarts", "Magic", "Hagrid"],
+  },
+  {
+    firstName: "Sherlock",
+    lastName: "Holmes",
+    number: "0487345643",
+    likes: ["Intriguing Cases", "Violin"],
+  },
+  {
+    firstName: "Kristian",
+    lastName: "Vos",
+    number: "unknown",
+    likes: ["JavaScript", "Gaming", "Foxes"],
+  },
+];
+
+function lookUpProfile(name, prop) {
+  // Only change code below this line
+for (let i = 0; i < contacts.length; i++) {
+		if (contacts[i].firstName === name) {
+			if (prop in contacts[i]) {
+				return contacts[i][prop];
+			}
+			else return "No such property";
+		}		
+	}
+	return ("No such contact");
+  // Only change code above this line
+}
+
+lookUpProfile("Akira", "likes");
 
 // generate-random-fractions-with-javascript
 
@@ -88,8 +140,25 @@ function randomRange(myMin, myMax) {
   return Math.floor(Math.random() * (myMax - myMin + 1)) + myMin; 
 }
 
-// 
+// use-the-parseint-function
+// Используйте parseInt()в convertToIntegerфункции, чтобы она преобразовывала входную строку strв целое число и возвращала его.
 
+function convertToInteger(str) {
+  return parseInt(str);
+}
+
+convertToInteger("007");
+
+// use-the-parseint-function-with-a-radix
+// Используйте parseInt()в convertToIntegerфункции, чтобы она преобразовывала двоичное число в целое и возвращала его.
+
+function convertToInteger(str) {
+ return parseInt(str, 2);
+}
+
+convertToInteger("10011");
+
+// 
 
 
 
