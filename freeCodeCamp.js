ebc5a917fc6fb96defbee95e380348b630cd7aa3
Vasilievkosta <vasilievkosta@gmail.com>
2021-11-19 18:54:02 +0200

    add tasks buildOfCubes

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -452,12 +452,71 @@ const stats = {
 };
 const half = ({max, min}) => (max + min) / 2.0; 
 
+// create-strings-using-template-literals
+
+//Используйте метод итератора (любой цикл), чтобы получить желаемый результат (как показано ниже).
+
+//[
+//  '<li class="text-warning">no-var</li>',
+// '<li class="text-warning">var-on-top</li>',
+//  '<li class="text-warning">linebreak</li>'
+//]
+const result = {
+  success: ["max-length", "no-amd", "prefer-arrow-functions"],
+  failure: ["no-var", "var-on-top", "linebreak"],
+  skipped: ["no-extra-semi", "no-dup-keys"]
+};
+function makeList(arr) {
+  // Only change code below this line
+  const failureItems = [];
+  for (let i = 0; i < arr.length; i++) {
+    failureItems.push(`<li class="text-warning">${arr[i]}</li>`);
+  }
+  // Only change code above this line
+
+  return failureItems;
+}
+
+const failuresList = makeList(result.failure);
+
+// write-concise-object-literal-declarations-using-object-property-shorthand
+
+const createPerson = (name, age, gender) => ({  
+  name, age, gender  
+});
+
+// createPerson("Zodiac Hasbro", 56, "male");
+// вернет {name: "Zodiac Hasbro", age: 56, gender: "male"}
+
+//При определении функций внутри объектов в ES5 мы должны использовать ключевое слово functionследующим образом:
+
+const person = {
+  name: "Taylor",
+  sayHello: function() {
+    return `Hello! My name is ${this.name}.`;
+  }
+};
+//В ES6 вы можете полностью удалить functionключевое слово и двоеточие при определении функций в объектах. Вот пример этого синтаксиса:
+
+const person = {
+  name: "Taylor",
+  sayHello() {
+    return `Hello! My name is ${this.name}.`;
+  }
+};
+
+// write-concise-declarative-functions-with-es6
+const bicycle = {
+  gear: 2,
+  setGear(newGear) {
+    this.gear = newGear;
+  }
+};
+
+bicycle.setGear(3);
+console.log(bicycle.gear);
+
 // 
-
-
-
-
-
 
 
 
