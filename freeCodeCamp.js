d9d0433f695d93ea88c853268ad198247b1b9311
Vasilievkosta <vasilievkosta@gmail.com>
2021-11-30 22:35:36 +0200

    finished the block called debugger

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -703,12 +703,32 @@ console.log(`Sum of array values is: ${arraySum}`);
 // Debager
 
 // catch-mixed-usage-of-single-and-double-quotes
-let innerHtml = '<p>Click here to <a href="#Home">return home</a></p>';
+let innerHtml = "<p>Click here to <a href='#Home'>return home</a></p>";
 console.log(innerHtml);
 
+// use-caution-when-reinitializing-variables-inside-a-loop
+
+function zeroArray(m, n) {
+  // Creates a 2-D array with m rows and n columns of zeroes
+  let newArray = [];
+  
+  for (let i = 0; i < m; i++) {
+    // Adds the m-th row into newArray
+let row = [];
+    for (let j = 0; j < n; j++) {
+      // Pushes n zeroes into the current row to create the columns
+      row.push(0);
+    }
+    // Pushes the current row, which now has n zeroes in it, to the array
+    newArray.push(row);
+  }
+  return newArray;
+}
+
+let matrix = zeroArray(3, 2);
+console.log(matrix);
+
 // 
-
-
 
 
 
